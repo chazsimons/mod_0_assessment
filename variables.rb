64be15e192126acf4f6221eba7324826dfcb7f54
Chaz Simons <chaz.simons@gmail.com>
2021-07-22 19:18:06 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+famous_person = "Adam Sandler"
+is_active = true
+
+famous_person = "Rob Schnider"
+num_3 = 5
+num_9 = 70
+
+sum = num_3 + num_9
+
+animals = ["horse", "monkey", "parrot"]
+
+animals.delete_at(0)
+p famous_person.length
