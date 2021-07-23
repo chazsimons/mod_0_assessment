eb965a158ecfa948fcd24478b21b9c59ba2963d5
Chaz Simons <chaz.simons@gmail.com>
2021-07-22 19:24:47 -0700

    Complete model_a challenge

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school ={
+  name: "Turing School",
+  program: "Backend Engineering",
+  class_size: 15,
+  number_of_instructors: 100,
+  remote_option: true,
+  accredited: true,
+  locations: ["Denver", "Las Vegas", "Remote Option"],
+  extracurriculars: ["Film Club", "Ghost Hunters", "Comic Readers"]
+}
