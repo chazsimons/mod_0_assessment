1496d5d4085520b43077c04a9a76b3f50b39b19c
Chaz Simons <chaz.simons@gmail.com>
2021-07-22 19:30:11 -0700

    Fix boolean syntax

--- models/model_a.rb
+++ models/model_a.rb
@@ -3,8 +3,8 @@ school ={
   program: "Backend Engineering",
   class_size: 15,
   number_of_instructors: 100,
-  remote_option: true,
-  accredited: true,
+  has_remote_option: true,
+  is_accredited: true,
   locations: ["Denver", "Las Vegas", "Remote Option"],
   extracurriculars: ["Film Club", "Ghost Hunters", "Comic Readers"]
 }
