844c074063cde0191630f899327582f7ce56d7e0
Chaz Simons <chaz.simons@gmail.com>
2021-07-22 19:29:38 -0700

    Complete model_b challenge

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  name: "Tanuki",
+  favorite_food: "Tuna",
+  age: 6,
+  weight_in_lbs: 8.5,
+  is_fluffy: true,
+  is_sweet: true,
+  favorite_toys: ["piece of string", "floppy fish", "catnip mouse"],
+  color_of_fur: ["White", "Light Brown", "Little bit of grey"]
+}
